2c47f91a9b4b78011bc9fd0d66f21f55dda50dec
Kacper Leśniański <kacper.lesnianski@wp.pl>
2022-05-09 18:02:25 +0200

    Random placement

--- src/graphics.py
+++ src/graphics.py
@@ -8,7 +8,7 @@ def get_bounds(blocks, scale=1.0):
     return (max_x, max_y)
 
 
-def draw_block(block, img_size, img_d, scale=1.0):
+def draw_block(block, img_size, img_d, scale=1.0, fontscale=1.0):
     # Invert Y coordinate, because for pil 0,0 is top left corner of screen,
     top_left_x = int(block.positionX * scale)
     top_left_y = int(img_size[1] - (block.positionY + block.height) * scale)
@@ -20,18 +20,18 @@ def draw_block(block, img_size, img_d, scale=1.0):
     center_x = top_left_x + (bottom_right_x - top_left_x) / 2
     center_y = top_left_y + (bottom_right_y - top_left_y) / 2
 
-    fnt = ImageFont.truetype("OpenSans-Bold.ttf", int(base_font_size * scale))
+    fnt = ImageFont.truetype("OpenSans-Bold.ttf", int(base_font_size * fontscale))
 
     w, h = img_d.textsize(block.name, font=fnt)
     img_d.text((center_x - (w / 2), center_y - (h / 2)), block.name, fill="black", font=fnt)
 
 
-def placement_visualisation(filename, blocks, connections=None, scale=1.0):
+def placement_visualisation(filename, blocks, connections=None, scale=1.0, fontscale=1.0):
     img_size = get_bounds(blocks, scale)
     image = Image.new("RGB", img_size, color=(192, 192, 192))
     img_d = ImageDraw.Draw(image)
 
     for block in blocks:
-        draw_block(block, img_size, img_d, scale)
+        draw_block(block, img_size, img_d, scale, fontscale)
 
     image.save(filename)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
+import random
 import files
 import graphics
 import binarytree
 
+
 def log(msg):
     print(msg)
+
 
 def check_if_feasible(blocks):
     # check if blocks don't collide
@@ -29,6 +32,51 @@ def check_if_feasible(blocks):
     return True
 
 
+def set_block_position(blocks, name, positionX, positionY):
+    for block in blocks:
+        if block.name == name:
+            block.positionX = positionX
+            block.positionY = positionY
+
+
+def get_block_by_name(blocks, name):
+    for block in blocks:
+        if block.name == name:
+            return block
+
+
+def visit_node(blocks, node, parent, is_left_child):
+    if node is None:
+        return
+
+    parent_block = get_block_by_name(blocks, parent.value)
+
+    if is_left_child:
+        posX = parent_block.positionX + parent_block.width
+        posY = parent_block.positionY
+
+        set_block_position(blocks, node.value, posX, posY)
+    else:
+        posX = parent_block.positionX
+        posY = parent_block.positionY + parent_block.height
+
+        set_block_position(blocks, node.value, posX, posY)
+
+    visit_node(blocks, node.left, node, True)
+    visit_node(blocks, node.right, node, False)
+
+
+def place_blocks(tree_root, blocks):
+    new_blocks = blocks.copy()
+
+    r_name = tree_root.value
+    set_block_position(new_blocks, r_name, 0, 0)
+
+    visit_node(blocks, tree_root.left, tree_root, True)
+    visit_node(blocks, tree_root.right, tree_root, False)
+
+    return new_blocks
+
 if __name__ == "__main__":
 
     try:
@@ -36,16 +84,22 @@ if __name__ == "__main__":
     except:
         exit()
 
-    graphics.placement_visualisation("results.png", blocks, scale=10)
+    graphics.placement_visualisation("results.png", blocks, scale=0.1, fontscale=1.0)
 
-    check_if_feasible(blocks)
+    while True:
+        block_names = [block.name for block in blocks]
+        random.shuffle(block_names)
 
-    block_names = [block.name for block in blocks]
+        binary_tree = binarytree.build(block_names)
 
-    # print(block_names)
+        nb = place_blocks(binary_tree, blocks)
 
-    binary_tree = binarytree.build(block_names)
+        if(check_if_feasible(blocks)):
+            break
+
+    graphics.placement_visualisation("results_2.png", nb, scale=0.1, fontscale=1.0)
+
     print("Binary tree from list :\n", binary_tree)
 
-    for block in blocks:
-        print(block.toJSON())
+    # for block in blocks:
+    #     print(block.toJSON())
