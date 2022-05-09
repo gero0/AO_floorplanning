9a318e2fa49770d5a7ebb9da8670b1f30e1c745b
Kacper Leśniański <kacper.lesnianski@wp.pl>
2022-05-09 11:03:53 +0200

    add collision checking function

--- src/main.py
+++ src/main.py
@@ -2,6 +2,32 @@ import files
 import graphics
 import binarytree
 
+def log(msg):
+    print(msg)
+
+def check_if_feasible(blocks):
+    # check if blocks don't collide
+    for block in blocks:
+        for block2 in blocks:
+            if block == block2:
+                continue
+
+            if (
+                block.positionX < block2.positionX + block2.width
+                and block.positionX + block.width > block2.positionX
+                and block.positionY < block2.positionY + block2.height
+                and block.height + block.positionY > block2.positionY
+            ):
+                log(
+                    "Solution unfeasible. Collision between blocks {} and {}".format(
+                        block.name, block2.name
+                    )
+                )
+
+                return False
+
+    return True
+
 
 if __name__ == "__main__":
 
@@ -10,16 +36,16 @@ if __name__ == "__main__":
     except:
         exit()
 
-    graphics.placement_visualisation("results.png", blocks)
+    graphics.placement_visualisation("results.png", blocks, scale=10)
+
+    check_if_feasible(blocks)
 
     block_names = [block.name for block in blocks]
 
-    print(block_names)
+    # print(block_names)
 
     binary_tree = binarytree.build(block_names)
-    print('Binary tree from list :\n',
-      binary_tree)
- 
+    print("Binary tree from list :\n", binary_tree)
 
-    # for block in blocks:
-    #     print(block.toJSON())
+    for block in blocks:
+        print(block.toJSON())
